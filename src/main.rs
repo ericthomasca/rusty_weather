86ff550adffd028f88d121460ede0a921893007a
Eric Thomas <eric@ericthomas.ca>
2022-09-18 16:12:15 -0230

    weather json to rust serde format

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,103 @@
-extern crate dotenv;
-
 use dotenv::dotenv;
 use std::env;
+use serde::Deserialize;
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use reqwest::Error;
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct WeatherData {
+    pub coord: Coord,
+    pub weather: Vec<Weather>,
+    pub base: String,
+    pub main: Main,
+    pub visibility: i64,
+    pub wind: Wind,
+    pub clouds: Clouds,
+    pub dt: i64,
+    pub sys: Sys,
+    pub timezone: i64,
+    pub id: i64,
+    pub name: String,
+    pub cod: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Coord {
+    pub lon: f64,
+    pub lat: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Weather {
+    pub id: i64,
+    pub main: String,
+    pub description: String,
+    pub icon: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Main {
+    pub temp: f64,
+    #[serde(rename = "feels_like")]
+    pub feels_like: f64,
+    #[serde(rename = "temp_min")]
+    pub temp_min: f64,
+    #[serde(rename = "temp_max")]
+    pub temp_max: f64,
+    pub pressure: i64,
+    pub humidity: i64,
+    #[serde(rename = "sea_level")]
+    pub sea_level: i64,
+    #[serde(rename = "grnd_level")]
+    pub grnd_level: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Wind {
+    pub speed: f64,
+    pub deg: i64,
+    pub gust: f64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Clouds {
+    pub all: i64,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Sys {
+    #[serde(rename = "type")]
+    pub type_field: i64,
+    pub id: i64,
+    pub country: String,
+    pub sunrise: i64,
+    pub sunset: i64,
+}
+
 
 fn main() {
+    // Import .env file
     dotenv().ok();
 
+    // Create weather api url
     let city_url_prefix = "https://api.openweathermap.org/data/2.5/weather?appid=";
     let owm_api = &env::var("OPEN_WEATHER_MAP_API").unwrap() as &str;
     let query_key = "&q=";
     let city = "corner&20brook";
-
     let weather_url = city_url_prefix.to_owned() + owm_api + query_key + city;
 
-    println!("{}", weather_url);
+    println!("{}", weather_url); // to print the complete request url
+
+
+
 }
 
 fn timestamp_to_datetime(epoch: i32) -> i32 {
