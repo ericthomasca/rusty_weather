4a0adf384d1773e14170179e2afd150851847694
Eric Thomas <eric@ericthomas.ca>
2022-09-18 00:30:43 -0230

    placeholders for functions

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,28 @@ fn main() {
     let weather_url = city_url_prefix.to_owned() + owm_api + query_key + city;
 
     println!("{}", weather_url);
+}
 
+fn timestamp_to_datetime(epoch: i32) -> i32 {
+    epoch
+}
 
+fn timestamp_to_time(epoch: i32) -> i32 {
+    epoch
+}
+
+fn kelvin_to_celcius(kelvin_temp: f64) -> i32 {
+    kelvin_temp as i32
+}
+
+fn deg_to_cardinal(deg: f64) -> f64 {
+    let dirs = [
+        "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE", 
+        "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW",
+    ];
+    deg
+}
+
+fn mps_to_kmph(mps: f64) -> i32 {
+    mps as i32
 }
